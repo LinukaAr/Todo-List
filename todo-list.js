36ab8b6982da37de35591d3b4941a6855b967086
LinukaAr <linukaarambawela@gmail.com>
2023-10-03 16:32:01 +0530

    Delete button/ Duedate

--- todo-list.js
+++ todo-list.js
@@ -1,13 +1,21 @@
-const todoList = [];
+const todoList = [{name:'',dueDate:''}];
 renderTodoList();
+
 function renderTodoList() {
-    
-
     let todoListHTML = '';
-
     for (let i = 0; i < todoList.length; i++) {
-        const todo = todoList[i];
-        const html = `<p>${todo}</p>`;
+        const todoObject = todoList[i];
+        //const name=todoObject.name;
+        //const dueDate=todoObject.dueDate;
+        const {name, dueDate}=todoObject;
+        
+        const html = `
+        <p>
+        ${name} ${dueDate} 
+        <button onclick="todoList.splice(${i}, 1);
+        renderTodoList();
+        ">Delete</button>
+        </p>`;
         todoListHTML += html;
     }
 
@@ -20,7 +28,9 @@ function renderTodoList() {
 function addTodo() {
     const inputEl = document.querySelector('.inputTodo');
     const name = inputEl.value;
-    todoList.push(name);
+    const dateInputEl=document.querySelector('.date-input');
+    const dueDate = dateInputEl.value;
+    todoList.push({name, dueDate});
     console.log(todoList);
 
     inputEl.value = '';
